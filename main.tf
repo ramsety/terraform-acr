8f2136a137d9e234e17a50b2b5c292afb7694c2c
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-24 04:19:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,21 +12,21 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg_rmsty01" {
-  name     = "rg_rmsty01"
+resource "azurerm_resource_group" "eval-rg" {
+  name     = "eval-rg"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "rmsty01_vnet" {
   name                = "rmsty01_vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg_rmsty01.location
-  resource_group_name = azurerm_resource_group.rg_rmsty01.name
+  location            = azurerm_resource_group.eval-rg.location
+  resource_group_name = azurerm_resource_group.eval-rg.name
 }
 
 resource "azurerm_subnet" "endpoint" {
   name                 = "endpoint"
-  resource_group_name  = azurerm_resource_group.rg_rmsty01.name
+  resource_group_name  = azurerm_resource_group.eval-rg.name
   virtual_network_name = azurerm_virtual_network.rmsty01_vnet.name
   address_prefix       = "10.0.2.0/24"
 
@@ -35,16 +35,16 @@ resource "azurerm_subnet" "endpoint" {
 
 resource "azurerm_container_registry" "rmsty01acr" {
   name                     = "rmsty01acr"
-  resource_group_name      = azurerm_resource_group.rg_rmsty01.name
-  location                 = azurerm_resource_group.rg_rmsty01.location
+  resource_group_name      = azurerm_resource_group.eval-rg.name
+  location                 = azurerm_resource_group.eval-rg.location
   sku                      = "Premium"
   admin_enabled            = false
 }
 
 resource "azurerm_private_endpoint" "rmsty01_pe" {
   name                = "rmsty01_pe"
-  location            = azurerm_resource_group.rg_rmsty01.location
-  resource_group_name = azurerm_resource_group.rg_rmsty01.name
+  location            = azurerm_resource_group.eval-rg.location
+  resource_group_name = azurerm_resource_group.eval-rg.name
   subnet_id           = azurerm_subnet.endpoint.id
 
   private_service_connection {
