894257b4cc45ba9265411dc66e7e9aa4d22f59d5
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-23 20:43:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ backend "azurerm" {
     container_name        = "tfstate"
     key                   = "BXNSxGxwugWitklqE6wOTSe2+PkxGPgNtriZrLgkY6tiwaCzkr/owQS8UUmfoZzSwLK2yJckMXMgXxz0P2qAEw=="
 }
+}
 provider "azurerm" {
   version = ">=2.0"
   # The "feature" block is required for AzureRM provider 2.x.
   features {}
-}
 }
 resource "azurerm_container_registry" "acr" {
   name                     = var.ACR_NAME
