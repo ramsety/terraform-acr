527b6501a1b9c62efc3d242fc42ad60bc857dc09
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-24 03:21:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,5 +18,10 @@ resource "azurerm_container_registry" "acr" {
   sku                      = "Premium"
   admin_enabled            = false
   georeplication_locations = ["West Europe"]
- 
+
+network_rule_set {
+    default_action             = "Allow"
+    bypass                     = ["AzureServices"]
+    virtual_network_subnet_ids = ["/subscriptions/8d499e57-3675-4d06-ae05-1fe9de2b0f16/resourceGroups/eval-rg/providers/Microsoft.Network/virtualNetworks/eval-vnet-01/subnets/subnet-01", "/subscriptions/8d499e57-3675-4d06-ae05-1fe9de2b0f16/resourceGroups/eval-rg/providers/Microsoft.Network/virtualNetworks/eval-vnet-01/subnets/subnet-02", "/subscriptions/8d499e57-3675-4d06-ae05-1fe9de2b0f16/resourceGroups/eval-rg/providers/Microsoft.Network/virtualNetworks/eval-vnet-01/subnets/subnet-03"]
+}
 }
