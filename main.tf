c4d9a8e6ff7e716d302d3534c695b149f96f1b63
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-24 03:47:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+backend "azurerm" {
+    resource_group_name   = var.RG_NAME
+    storage_account_name  = "tfstateac"
+    container_name        = "tfstate"
+    key                   = "BXNSxGxwugWitklqE6wOTSe2+PkxGPgNtriZrLgkY6tiwaCzkr/owQS8UUmfoZzSwLK2yJckMXMgXxz0P2qAEw=="
+}
+}
+provider "azurerm" {
+  version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
+
 resource "azurerm_resource_group" "rg_rmsty01" {
   name     = "rg_rmsty01"
   location = "East US"
