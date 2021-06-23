9ef7aa7ec3a3c8931f85a3f903d43b1307c81446
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-24 03:45:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,42 @@
-terraform {
-backend "azurerm" {
-    resource_group_name   = var.RG_NAME
-    storage_account_name  = "tfstateac"
-    container_name        = "tfstate"
-    key                   = "BXNSxGxwugWitklqE6wOTSe2+PkxGPgNtriZrLgkY6tiwaCzkr/owQS8UUmfoZzSwLK2yJckMXMgXxz0P2qAEw=="
+resource "azurerm_resource_group" "rg_rmsty01" {
+  name     = "rg_rmsty01"
+  location = "East US"
 }
+
+resource "azurerm_virtual_network" "rmsty01_vnet" {
+  name                = "rmsty01_vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg_rmsty01.location
+  resource_group_name = azurerm_resource_group.rg_rmsty01.name
 }
-provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
-  features {}
+
+resource "azurerm_subnet" "endpoint" {
+  name                 = "endpoint"
+  resource_group_name  = azurerm_resource_group.rg_rmsty01.name
+  virtual_network_name = azurerm_virtual_network.rmsty01_vnet.name
+  address_prefix       = "10.0.2.0/24"
+
+  enforce_private_link_endpoint_network_policies = true
 }
-resource "azurerm_container_registry" "acr" {
-  name                     = var.ACR_NAME
-  resource_group_name      = "eval-rg"
-  location                 = "East US"
+
+resource "azurerm_container_registry" "rmsty01_acr" {
+  name                     = "rmsty01_acr"
+  resource_group_name      = azurerm_resource_group.rg_rmsty01.name
+  location                 = azurerm_resource_group.rmsty01_vnet.location
   sku                      = "Premium"
   admin_enabled            = false
-  georeplication_locations = ["West Europe"]
-
-network_rule_set {
-    default_action             = "Allow"
-    ip_rules       = ["10.0.0.0/24", "10.1.0.0/24", "10.2.0.0/24"]
 }
+
+resource "azurerm_private_endpoint" "rmsty01_pe" {
+  name                = "rmsty01_pe"
+  location            = azurerm_resource_group.rg_rmsty01.location
+  resource_group_name = azurerm_resource_group.rg_rmsty01.name
+  subnet_id           = azurerm_subnet.endpoint.id
+
+  private_service_connection {
+    name                           = "rmsty01_acr-privateserviceconnection"
+    private_connection_resource_id = azurerm_container_registry.rmsty01_acr.id
+    subresource_names              = ["registry"]
+    is_manual_connection           = false
+  }
 }
