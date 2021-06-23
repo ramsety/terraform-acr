9891555972e3b32ff0e16cbc0649e75edf9f4215
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-24 04:46:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,22 +12,22 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "eval-rg" {
-  name     = "eval-rg"
+resource "azurerm_resource_group" "rg-evalrmsty001" {
+  name     = "rg-evalrmsty001"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "rmsty01_vnet" {
-  name                = "rmsty01_vnet"
+resource "azurerm_virtual_network" "rmsty01_vnet01" {
+  name                = "rmsty01_vnet01"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.eval-rg.location
-  resource_group_name = azurerm_resource_group.eval-rg.name
+  location            = azurerm_resource_group.rg-evalrmsty001.location
+  resource_group_name = azurerm_resource_group.rg-evalrmsty001.name
 }
 
 resource "azurerm_subnet" "endpoint" {
   name                 = "endpoint"
-  resource_group_name  = azurerm_resource_group.eval-rg.name
-  virtual_network_name = azurerm_virtual_network.rmsty01_vnet.name
+  resource_group_name  = azurerm_resource_group.rg-evalrmsty001.name
+  virtual_network_name = azurerm_virtual_network.rmsty01_vnet01.name
   address_prefix       = "10.0.2.0/24"
 
   enforce_private_link_endpoint_network_policies = true
@@ -35,16 +35,16 @@ resource "azurerm_subnet" "endpoint" {
 
 resource "azurerm_container_registry" "rmsty01acr" {
   name                     = "rmsty01acr"
-  resource_group_name      = azurerm_resource_group.eval-rg.name
-  location                 = azurerm_resource_group.eval-rg.location
+  resource_group_name      = azurerm_resource_group.rg-evalrmsty001.name
+  location                 = azurerm_resource_group.rg-evalrmsty001.location
   sku                      = "Premium"
   admin_enabled            = false
 }
 
 resource "azurerm_private_endpoint" "rmsty01_pe" {
   name                = "rmsty01_pe"
-  location            = azurerm_resource_group.eval-rg.location
-  resource_group_name = azurerm_resource_group.eval-rg.name
+  location            = azurerm_resource_group.rg-evalrmsty001.location
+  resource_group_name = azurerm_resource_group.rg-evalrmsty001.name
   subnet_id           = azurerm_subnet.endpoint.id
 
   private_service_connection {
