ba935150ef7f84770d441fac720017c73ae3d250
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-23 20:25:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,11 @@ backend "azurerm" {
     container_name        = "tfstate"
     key                   = "BXNSxGxwugWitklqE6wOTSe2+PkxGPgNtriZrLgkY6tiwaCzkr/owQS8UUmfoZzSwLK2yJckMXMgXxz0P2qAEw=="
 }
-required_providers {
-    azurerm = {
-      # Specify what version of the provider we are going to utilise
-      source = "hashicorp/azurerm"
-      version = ">= 2.4.1"
-    }
-  }
+provider "azurerm" {
+  version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
 }
 resource "azurerm_container_registry" "acr" {
   name                     = var.ACR_NAME
